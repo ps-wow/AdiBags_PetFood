cc017dc5b18cfcce0c4d3f010ad4ae2c86817f76
davidcraig <davidcraig1986@gmail.com>
2019-06-09 08:45:32 +0100

    Add more food

--- main.lua
+++ main.lua
@@ -8,134 +8,154 @@ local AdiBags_PetFood = LibStub("AceAddon-3.0"):NewAddon("AdiBags_PetFood")
 AdiBags_PetFood.kbDEBUG = true
 function AdiBags_PetFood:Dump(str, obj)
   if ViragDevTool_AddData and AdiBags_PetFood.kbDEBUG then 
-      ViragDevTool_AddData(obj, str) 
+    ViragDevTool_AddData(obj, str) 
   end
 end
 
 AddonTable.ItemTables = {
-    fruit = {},
-    meat = {
-        ['117']  = true, -- Tough Jerky
-        ['769']  = true, -- Chunk of Boar Meat
-        ['2672'] = true, -- Stringy Wolf Meat
-        ['2679'] = true, -- Charred Wolf Meat
-        ['2680'] = true, -- Spiced Wolf Meat
-        ['2681'] = true, -- Roasted Boar Meat
-        ['2886'] = true, -- Crag Boar Rib
-    },
-    fish = {},
-    fungus = {},
-    misc = {},
+  fruit = {
+    ['4537']  = true, -- Tel'abim Banana
+  },
+  meat = {
+    ['117']   = true, -- Tough Jerky
+    ['724']   = true, -- Goretusk Liver Pie [Cooking]
+    ['729']   = true, -- Stringy Vulture Meat
+    ['769']   = true, -- Chunk of Boar Meat
+    ['2672']  = true, -- Stringy Wolf Meat
+    ['2677']  = true, -- Boar Ribs
+    ['2679']  = true, -- Charred Wolf Meat
+    ['2680']  = true, -- Spiced Wolf Meat [Cooking]
+    ['2681']  = true, -- Roasted Boar Meat [Cooking]
+    ['2687']  = true, -- Dry Pork Ribs [Cooking]
+    ['2886']  = true, -- Crag Boar Rib
+    ['3685']  = true, -- Raptor Egg (is meat?)
+    ['4655']  = true, -- Giant Clam Meat
+    ['5469']  = true, -- Strider Meat
+    ['5503']  = true, -- Clam Meat
+    ['5504']  = true, -- Tangy Clam Meat
+    ['7974']  = true, -- Zesty Clam Meat
+    ['15924'] = true, -- Soft-shelled Clam Meat
+  },
+  fish = {
+    ['6290']  = true, -- Brilliant Smallfish [Cooking]
+    ['6291']  = true, -- Raw Brilliant Smallfish
+  },
+  fungus = {},
+  bread = {
+    ['4540']  = true, -- Tough Hunk of Bread
+    ['4541']  = true, -- Freshly Baked Bread
+  },
+  misc = {},
 }
 AddonTable.Modules = {}
 
 function AdiBags_PetFood:GetOptions()
-    return {
-        meat = {
-            name = "meat",
-            desc = 'Meat',
-            type = 'toggle',
-            order = 70,
-        },
-        fish = {
-            name = "fish",
-            desc = 'Fish',
-            type = 'toggle',
-            order = 70,
-        },
-        fungus = {
-            name = "fungus",
-            desc = 'Fungus',
-            type = 'toggle',
-            order = 70,
-        },
-        fruit = {
-            name = "fruit",
-            desc = 'Fruit',
-            type = 'toggle',
-            order = 70,
-        },
-        bread = {
-            name = "bread",
-            desc = 'Bread',
-            type = 'toggle',
-            order = 70,
-        },
-        misc = {
-            name = "misc",
-            desc = 'Misc',
-            type = 'toggle',
-            order = 70,
-        },
-    }
+  return {
+    meat = {
+      name = "meat",
+      desc = 'Meat',
+      type = 'toggle',
+      order = 70,
+    },
+    fish = {
+      name = "fish",
+      desc = 'Fish',
+      type = 'toggle',
+      order = 70,
+    },
+    fungus = {
+      name = "fungus",
+      desc = 'Fungus',
+      type = 'toggle',
+      order = 70,
+    },
+    fruit = {
+      name = "fruit",
+      desc = 'Fruit',
+      type = 'toggle',
+      order = 70,
+    },
+    bread = {
+      name = "bread",
+      desc = 'Bread',
+      type = 'toggle',
+      order = 70,
+    },
+    misc = {
+      name = "misc",
+      desc = 'Misc',
+      type = 'toggle',
+      order = 70,
+    },
+  }
 end
 
 function AdiBags_PetFood:GetProfile()
-    return {
-        PetFood = true
-    }
+  return {
+    PetFood = true
+  }
 end
 
 function AdiBags_PetFood:DefaultFilter(slotData, module, petFoodFilter)
-    local prefix = module.prefix
+  local prefix = module.prefix
 
-    local ItemTables = AddonTable.ItemTables
+  local ItemTables = AddonTable.ItemTables
 
-    for categoryName, categoryDesc in pairs(ItemTables) do
-        if (petFoodFilter.db.profile[categoryName]) then
-            for id,_ in pairs(ItemTables[categoryName]) do
-                if (tonumber(id) == slotData.itemId) then
-                    --return "Pet Food ("..categoryName..")"
-                    return "Pet Food"
-                end
-            end
+  for categoryName, categoryDesc in pairs(ItemTables) do
+    if (petFoodFilter.db.profile[categoryName]) then
+      for id,_ in pairs(ItemTables[categoryName]) do
+        if (tonumber(id) == slotData.itemId) then
+          --return "Pet Food ("..categoryName..")"
+          return "Pet Food"
         end
+      end
     end
+  end
 end
 
 function AdiBags_PetFood:GetDefaultCategories()
-    return {
-        ["Meat"] = "Meat",
-        ["Fish"] = "Fish",
-        ["Fruit"] = "Fruit",
-        ["Fungus"] = "Fungus",
-        ["Bread"] = "Bread",
-        ["Misc"] = "Misc",
-    }
+  return {
+    ["Meat"] = "Meat",
+    ["Fish"] = "Fish",
+    ["Fruit"] = "Fruit",
+    ["Fungus"] = "Fungus",
+    ["Bread"] = "Bread",
+    ["Misc"] = "Misc",
+  }
 end
 
 function AdiBags_PetFood:Load(module)
-    local petFoodFilter = AdiBags:RegisterFilter(module.namespace, 90)
-    petFoodFilter.uiName = module.namespace
-    petFoodFilter.uiDesc = module.description
+  local petFoodFilter = AdiBags:RegisterFilter(module.namespace, 90)
+  petFoodFilter.uiName = module.namespace
+  petFoodFilter.uiDesc = module.description
 
-    if module.filter ~=nil then
-        function petFoodFilter:Filter(slotData)
-            return module.filter(slotData)
-        end
-    else
-        function petFoodFilter:Filter(slotData)
-            return AdiBags_PetFood:DefaultFilter(slotData, module, petFoodFilter)
-        end
+  if module.filter ~=nil then
+    function petFoodFilter:Filter(slotData)
+      return module.filter(slotData)
     end
+  else
+    function petFoodFilter:Filter(slotData)
+      return AdiBags_PetFood:DefaultFilter(slotData, module, petFoodFilter)
+    end
+  end
 
-    function petFoodFilter:OnInitialize()
-        self.db = AdiBags.db:RegisterNamespace(module.namespace, {
-            profile = AdiBags_PetFood:GetProfile()
-        })
-    end
+  function petFoodFilter:OnInitialize()
+    self.db = AdiBags.db:RegisterNamespace(module.namespace, {
+      profile = AdiBags_PetFood:GetProfile()
+    })
+  end
 
-    function petFoodFilter:GetOptions()
-        return AdiBags_PetFood:GetOptions(), AdiBags:GetOptionHandler(self, true)
-    end
+  function petFoodFilter:GetOptions()
+    return AdiBags_PetFood:GetOptions(), AdiBags:GetOptionHandler(self, true)
+  end
 end
 
 AddonTable.Modules.petfood = {
-    ["name"] = "petfood",
-    ["categories"] = AdiBags_PetFood:GetDefaultCategories(),
-    ["namespace"] = "Pet Food",
-    ["prefix"] = "Pet Food",
-    ["description"] = "Pet food"
+  ["name"] = "petfood",
+  ["categories"] = AdiBags_PetFood:GetDefaultCategories(),
+  ["namespace"] = "Pet Food",
+  ["prefix"] = "Pet Food",
+  ["description"] = "Pet food"
 }
 
 AdiBags_PetFood:Load(AddonTable.Modules.petfood)
